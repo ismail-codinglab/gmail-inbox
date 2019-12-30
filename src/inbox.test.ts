bcb3f6d9e8541833c9348bf6e76ebc7e23f8e86b
Ismail M <ismail.metaich@gmail.com>
2019-12-31 00:12:26 +0100

    fix tests

--- src/inbox.test.ts
+++
@@ -1,7 +0,0 @@
-import { Inbox } from './Inbox';
-describe('/diag/json', () => {
-  it('should be carl', () => {
-    const inbox = new Inbox('carl');
-    expect(inbox.sayMyName()).toBe('carl');
-  });
-});
